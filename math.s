31d32ec98c8c2c676d669e92907d1593a004eca9
harazat <74931256+harazat@users.noreply.github.com>
2021-12-09 12:38:00 +0400

    Update math.s

--- math.s
+++ math.s
@@ -1,38 +1,47 @@
 	.text
-	.globl	add
-	.type	add, @function
+  .globl  add
+  .type  add, @function
 add:
   # function prolog
-
+push %rbp
+mov %rsp, %rbp
 
   # do a + b and store the result in %rax
-
+add %rdi, %rsi
+mov %rsi, %rax
 
   # function epilog
-
-	.globl sub	
-	.type	sub, @function
+pop %rbp
+ret
+  .globl sub  
+  .type  sub, @function
 sub:
   # function prolog
-
+push %rbp
+mov %rsp, %rbp
 
   # do a - b and store the result in %rax
+sub %rsi, %rdi
+mov %rsi, %rax
 
 
   # function epilog
+pop %rbp
+ret
 
 
 
-
-	.globl mul
-	.type	mul, @function
+  .globl mul
+  .type  mul, @function
 mul:
   # function prolog
-
+push %rbp
+mov %rsp, %rbp
 
   # do a * b and store the result in %rax
-
+imul %rdi, %rsi
+mov %rsi, %rax
 
   # function epilog
-
-
+pop %rbp
+ret
